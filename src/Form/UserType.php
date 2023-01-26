5c6d1cd67c195c10903783eb1506e80481f67711
- <karina.teyssere@etu.univ-grenoble-alpes.fr>
2023-01-26 13:22:27 +0100

    Changement CSS

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,7 @@ class UserType extends AbstractType
             ->add('name')
             ->add('password', PasswordType::class)
             ->add('address')
-            ->add('mail')
+            ->add('mail', EmailType::class)
             //->add('roles')
             //->add('last_login')
         ;
